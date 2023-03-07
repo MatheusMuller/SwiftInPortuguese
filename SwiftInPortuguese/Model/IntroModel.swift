509cc4c14162f2898aae49c28c22761fbe8223e5
Matheus <mullerscalzer@hotmail.com>
2023-03-07 19:33:15 -0300

    Ajuste na struct de Introdução, alterada estrutura do Json de Introdução e adicionadas novas descrições, começo da organização da IntroView e adicionada nova imagem ao projeto.

--- SwiftInPortuguese/Model/IntroModel.swift
+++ SwiftInPortuguese/Model/IntroModel.swift
@@ -7,9 +7,9 @@
 
 import Foundation
 
-struct IntroModel: Identifiable {
-    let id: UUID
-    let topic: String
+struct IntroModel: Codable, Identifiable {
+    let id: String
     let description1: String
     let description2: String
+    let description3: String
 }
--- SwiftInPortuguese/View/IntroView.swift
+++ SwiftInPortuguese/View/IntroView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct IntroView: View {
+    let introContent: IntroModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                Text("Introdução")
+                    .font(.title.bold())
+                
+                Text(introContent.description1)
+                    .padding()
+                
+                Spacer(minLength: 50)
+                
+                Text("O que é o SWIFT?")
+                    .font(.title2)
+                
+                Image("swiftBanner1")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(introContent.description2)
+                    .padding()
+                
+                Spacer(minLength: 50)
+                
+                Text("O que preciso para começar?")
+                    .font(.title2)
+                
+                Text(introContent.description3)
+                    .padding()
+            }
+        }
+        .background(Color(red: 0.9, green: 0.9, blue: 0.9))
     }
 }
 
 struct IntroView_Previews: PreviewProvider {
+    static let introContents: [String: IntroModel] = Bundle.main.decode("IntroData.json")
+    
     static var previews: some View {
-        IntroView()
+        IntroView(introContent: introContents["intro"]!)
     }
 }
